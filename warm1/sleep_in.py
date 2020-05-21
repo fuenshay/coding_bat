2421b933681ccc6ef2536f4ea086b3f40251fb58
Shayna Fuentes <yourcreator@Shaynas-MBP.lan1>
2020-05-20 22:06:31 -0500

    This better be correct.

---
+++ warm1/sleep_in.py
@@ -0,0 +1,9 @@
+# def sleep_in(weekday, vacation):
+#   if not weekday or vacation:
+#     return True
+#   else:
+#     return False
+    
+def sleep_in(weekday, vacation):
+    return(not weekday or vacation)
+    
