de60f1bf0f18dea2ba2d9539dcd67441d456d8c5
fuenshay <fuenshay@gmail.com>
2020-05-22 14:10:46 -0500

    sum double

--- sleep_in.py
+++
@@ -1,9 +0,0 @@
-# def sleep_in(weekday, vacation):
-#   if not weekday or vacation:
-#     return True
-#   else:
-#     return False
-    
-def sleep_in(weekday, vacation):
-    return(not weekday or vacation)
-    
